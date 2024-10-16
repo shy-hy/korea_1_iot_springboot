3d1412e8e38c30794b27fa11c214cb898239a9a5
devgiants75 <devgiants75@naver.com>
2024-10-16 11:10:51 +0900

    20241016

---
+++ springboot-developer/src/test/java/JUnitTest.java
@@ -0,0 +1,32 @@
+import org.junit.jupiter.api.*;
+
+public class JUnitTest {
+    // @DsiplayName
+    // : 테스트 이름을 명시
+    @DisplayName("1 + 2는 3이다.")
+    // @Test
+    // : 해당 에너테이션을 붙인 메서드는 테스트를 수행하는 메서드
+    @Test
+    public void junitTest() {
+        int a = 1;
+        int b = 2;
+        int sum = 3;
+
+        // .assertEquals
+        // : JUnit에서 제공하는 검증 메서드
+        // : 첫 번째 인자 - 기대하는 값
+        // : 두 번째 인자 - "실제로" 검증할 값
+
+        Assertions.assertEquals(sum, a + b);
+    }
+
+//    @DisplayName("1 + 3는 4이다.")
+//    @Test
+//    public void junitFailedTest() {
+//        int a = 1;
+//        int b = 3;
+//        int sum = 3;
+//
+//        Assertions.assertEquals(sum, a + b);
+//    }
+}
